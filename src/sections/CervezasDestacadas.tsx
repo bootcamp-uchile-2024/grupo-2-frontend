2b980d081a4c7044391fdedf4e41b8d08a304c29
David Manriquez <dmanriquez@bcn.cl>
2024-11-27 16:40:00 -0300

    Se agregan imágenes de cervezas

---
+++ src/sections/CervezasDestacadas.tsx
@@ -0,0 +1,54 @@
+export const CervezasDestacadas = () => {
+  return (
+    <div className="section">
+      <div className="container mx-auto text-center py-10">
+        <h1 className="font-title text-6xl pb-8">Destacadas</h1>
+        <div className="flex flex-wrap justify-center gap-4 md:flex-nowrap">
+          <div className="w-100 md:w-1/3 flex flex-col shadow-lg">
+            <img className="w-100" src="assets/cerveza-1.png" alt="Nombre de la cerveza" />
+            <div className="p-4 flex flex-col items-start">
+              <p className="text-purple text-xl">Línea 1 | Amber Ale</p>
+              <p>Alameda Beer Co - Santiago</p>
+              <div>Estrellas</div>
+              <p className="font-semibold text-2xl mb-4">$4.500</p>
+              <button className="btn-carro">Agregar al carrito</button>
+            </div>
+          </div>
+          <div className="w-100 md:w-1/3 flex flex-col shadow-lg">
+            <img className="w-100" src="assets/cerveza-2.png" alt="Nombre de la cerveza" />
+            <div className="p-4 flex flex-col items-start">
+              <p className="text-purple text-xl">Línea 1 | Amber Ale</p>
+              <p>Alameda Beer Co - Santiago</p>
+              <div>Estrellas</div>
+              <p className="font-semibold text-2xl mb-4">$4.500</p>
+              <button className="btn-carro">Agregar al carrito</button>
+            </div>
+          </div>
+          <div className="w-100 md:w-1/3 flex flex-col shadow-lg">
+            <img className="w-100" src="assets/cerveza-3.png" alt="Nombre de la cerveza" />
+            <div className="p-4 flex flex-col items-start">
+              <p className="text-purple text-xl">Línea 1 | Amber Ale</p>
+              <p>Alameda Beer Co - Santiago</p>
+              <div>Estrellas</div>
+              <p className="font-semibold text-2xl mb-4">$4.500</p>
+              <button className="btn-carro">Agregar al carrito</button>
+            </div>
+          </div>
+          <div className="w-100 md:w-1/3 flex flex-col shadow-lg">
+            <img className="w-100" src="assets/cerveza-4.png" alt="Nombre de la cerveza" />
+            <div className="p-4 flex flex-col items-start">
+              <p className="text-purple text-xl">Línea 1 | Amber Ale</p>
+              <p>Alameda Beer Co - Santiago</p>
+              <div>Estrellas</div>
+              <p className="font-semibold text-2xl mb-4">$4.500</p>
+              <button className="btn-carro">Agregar al carrito</button>
+            </div>
+          </div>
+        </div>
+        <div className="pt-8">
+          <button className="">Quiero ver más!</button>
+        </div>
+      </div>
+    </div>
+  );
+}
