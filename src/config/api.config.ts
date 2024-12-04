63a915c533caf38695311b6e9bb3e85f62feca88
calvegh <calvegh@gmail.com>
2024-12-04 12:13:23 -0300

    cambios

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,5 +2,5 @@ export const API_URL = import.meta.env
   .VITE_SERVER_API; /* || 'http://localhost:3000' */
 
 // export const BOMBILLAS_ENDPOINT = `${API_URL}/api/bombillas`
-export const CERVEZAS_ENDPOINT = `${API_URL}/cervezas`;
-export const USERS_ENDPOINT = `${API_URL}/usuarios/`;
+export const CERVEZAS_ENDPOINT = `${API_URL}cervezas`;
+export const USERS_ENDPOINT = `${API_URL}usuarios/`;
