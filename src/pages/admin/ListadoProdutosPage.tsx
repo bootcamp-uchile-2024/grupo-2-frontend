bc8e35ece72c2dd4ca95ccc0f88accebe2767f55
David Manriquez <dmanriquez@bcn.cl>
2024-12-11 23:39:50 -0300

    Se suben archivos

---
+++ src/pages/admin/ListadoProdutosPage.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Cervezas } from "../../types";
+import { CERVEZAS_ENDPOINT } from "../../config/api.config";
+
+export const ListadoProductosPage = () => {
+  const [productos, setProductos] = useState<Cervezas[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // Funcion para obtener los productos de la base de datos
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const response = await fetch(`${CERVEZAS_ENDPOINT}?pagina=${1}&cantproductos`);
+        const data = await response.json();
+        setProductos(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProductos();
+  }, []);
+
+  // Funcion para actualizar el estado de la cerveza en la base de datos (activa o inactiva)
+  const updateCervezaStatus = async (id: number, isActive: boolean) => {
+    try {
+      const response = await fetch(`${CERVEZAS_ENDPOINT}/${id}/actualizarestado`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ is_active: isActive }),
+      });
+      if (response.ok) {
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+          const updatedProducto = await response.json();
+          setProductos((prevProductos) =>
+            prevProductos.map((producto) =>
+              producto.id === id ? updatedProducto : producto
+            )
+          );
+        } else {
+          console.log('La respuesta no es un JSON válido');
+        }
+      } else {
+        const errorText = await response.text();
+        console.log('Error al actualizar el estado de la cerveza:', errorText);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  // Funcion para cambiar el estado de la cerveza (activa o inactiva)
+  const handleButtonClick = async (producto: Cervezas) => {
+    await updateCervezaStatus(producto.id, !producto.is_active);
+    setProductos((prevProductos) =>
+      prevProductos.map((p) =>
+        p.id === producto.id ? { ...p, is_active: !producto.is_active } : p
+      )
+    );
+  };
+
+  return (
+    <div className="w-full p-8">
+      <h1 className="mb-4 font-lato  text-purple-100 text-custom-lg font-normal">Listado de cervezas</h1>
+        <div className="mt-4">
+          <Link to="/dashboard/crea-producto" className="btn-formulario">
+            Crear producto
+          </Link>
+        </div>
+        <div className="mt-4">
+          {loading ? (
+            <p>Cargando...</p>
+          ) : (
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-40">Imagen</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Descripción</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Precio</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Stock</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/5">Acciones</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {productos.map((producto) => (
+                  <tr key={producto.id} className={producto.is_active ? '' : 'bg-slate-200'}>
+                    <td className="px-6 py-4">
+                      <img src={producto.imagen ? producto.imagen.replace('./', '/') : '/assets/no-imagen.png'} alt={producto.nombre} className="w-100"/>
+                    </td>
+                    <td className="px-6 py-4"><Link to={`/dashboard/editar-producto/${producto.id}`}>{producto.nombre}</Link></td>
+                    <td className="px-6 py-4">{producto.descripcion}</td>
+                    <td className="px-6 py-4">{producto.precio}</td>
+                    <td className="px-6 py-4">{producto.stock}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right">
+                      <button onClick={() => handleButtonClick(producto)} className="btn-formulario mr-2"><i className="fas fa-power-off"></i> {producto.is_active ? 'Desactivar' : 'Activar'}</button>
+                      <button onClick={() => window.location.href = `/dashboard/editar-producto/${producto.id}`} className="btn-formulario"><i className="far fa-edit"></i> Editar</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+  );
+}
---
+++ src/pages/admin/ListadoUsuarioPage.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import { Usuario } from "../../types";
+import { Link } from "react-router-dom";
+import { USERS_ENDPOINT } from "@/config/api.config";
+
+export const ListadoUsuarioPage = () => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(USERS_ENDPOINT)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los usuarios");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsuarios(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los usuarios:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  const handleEliminar = async (rut: string) => {
+    try {
+      const response = await fetch(`${USERS_ENDPOINT}${rut}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Error eliminando el usuario");
+      }
+      alert("Usuario eliminado exitosamente");
+      // Aquí puedes agregar lógica para actualizar la lista de usuarios después de la eliminación
+      setUsuarios(usuarios.filter((usuario) => usuario.rut !== rut));
+    } catch (error) {
+      console.error("Error eliminando el usuario:", error);
+      alert("Hubo un error al eliminar el usuario");
+    }
+  };
+
+  return (
+    <div className="w-full p-8">
+      <h1 className="mb-4 font-lato  text-purple-100 text-custom-lg font-normal">
+        Listado de Usuarios
+      </h1>
+      <div className="mt-4">
+        <Link to="/dashboard/crea-usuario" className="btn-formulario">
+          Ingresar usuario
+        </Link>
+      </div>
+      <div className="mt-4">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Rut
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Nombre
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Apellido
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Email
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Teléfono
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Acción
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {usuarios.map((usuario) => (
+              <tr key={usuario.rut}>
+                <td className="px-6 py-4 whitespace-nowrap">{usuario.rut}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {usuario.nombre}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {usuario.apellido}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {usuario.correo_comprador}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {usuario.telefono_comprador}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <button
+                    className="btn-formulario-outline"
+                    onClick={() => handleEliminar(usuario.rut)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
