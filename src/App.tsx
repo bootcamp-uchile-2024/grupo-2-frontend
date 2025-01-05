bb70ab1cf47b3903ea94a6921ffcedf319dfe846
David Manriquez <dmanriquez@bcn.cl>
2025-01-05 18:13:51 -0300

    Creación de páginas en construcción y rediseño de la 404

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,10 @@ import { PagoPage } from "./pages/PagoPage";
 import { PasarelaPagoPage } from "./pages/PasarelaPagoPage";
 import { CompraExitosaPage } from "./pages/CompraExitosaPage";
 import { ClubPage } from "./pages/ClubPage";
+import { PacksPage } from "./pages/PacksPage";
+import { ContenidoEducativoPage } from "./pages/ContenidoEducativoPage";
+import { ComunidadPage } from "./pages/ComunidadPage";
+import { OfertasPage } from "./pages/OfertasPage";
 
 declare global {
   interface Window {
@@ -90,6 +94,10 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/resumen-carrito" element={<CarritoPage />} />
         <Route path="/club" element={<ClubPage />} />
+        <Route path="/packs" element={<PacksPage />} />
+        <Route path="/contenido" element={<ContenidoEducativoPage />} />
+        <Route path="/comunidad" element={<ComunidadPage />} />
+        <Route path="/ofertas" element={<OfertasPage />} />
         <Route path="*" element={<PageNoFound />} />
         <Route path="/admin" element={<AdminLoginPage />} />
         <Route
--- src/pages/ComunidadPage.tsx
+++ src/pages/ComunidadPage.tsx
@@ -1,13 +1,13 @@
 import { MainLayout } from "@/layout/MainLayout"
 
-export const PageNoFound = () => {
+export const ComunidadPage = () => {
   return (
     <MainLayout>
       <div className="container mx-auto text-center">
         <div className="flex flex-col items-center justify-center p-12">
-          <h1 className="text-5xl">404</h1>
-          <p className="mb-8">Pagina no encontrada.</p>
-          <img src="assets/img-pagina-no-encontrada.png" alt="404" /> 
+          <h1 className="text-5xl">En construcción</h1>
+          <p className="mb-8">Pronto nuevos contenidos.</p>
+          <img src="assets/img-sitio-en-construccion.png" alt="Sitio en Construcción"/> 
         </div>
       </div>
     </MainLayout>
